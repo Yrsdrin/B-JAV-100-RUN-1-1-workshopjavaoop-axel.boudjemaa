17ec9e973a642aaa92febd53a03776154e9a7eeb
Yrsdrin <axel.boudjemaa@epitech.eu>
2023-12-11 11:10:57 +0400

    Oui

--- Javaprogeam.java
+++ Javaprogeam.java
@@ -7,30 +7,9 @@
 
 public class Javaprogeam
 {
-    String Brand;
-    String Model;
-    String Year;
-    public static void accelerate()
-        {
-            System.out.println("I'm putting the pedal to the metal");
-        }
-    public static void brake()
-        {
-            System.out.println("Woah! Slow down boy!");
-	}
-    public void describe()
-        {
-            System.out.println("A " + Brand + " " + Model + " made in " + Year);
-	}
-    public Javaprogeam(String brand, String model, String year)
-        {
-            Brand = brand;
-            Model = model;
-            Year = year;
-        }
     public static void main(String []args)
         {
-            Javaprogeam car = new Javaprogeam(args[0], args[1], args[2]);
+            Vehicle car = new Vehicle(args[0], args[1], args[2]);
             car.describe();
             car.accelerate();
         }
